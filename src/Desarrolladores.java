83200a4df36125ccb249d410162e7b1d728f4eac
ret16339 <ret16339@uvg.edu.gt>
2017-03-07 00:08:01 -0600

    Primeras Clases

--- src/Desarrolladores.java
+++ src/Desarrolladores.java
@@ -1,4 +1,32 @@
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.LinkedHashSet;
+
+import java.util.Scanner;
 
 public class Desarrolladores {
+
+	public static void main(String[] args) throws IOException {
+		Scanner s = new Scanner(System.in);
+		FactoryDesarrollo<String> FactoryDesarrolladores = new FactoryDesarrollo<String>();
+		
+		
+		System.out.println("Bienvendio al programa de Desarrolladores");
+		
+		//Java
+		System.out.println("Presione 1 para usar HashSet");
+		
+		//Android
+		System.out.println("Presione 2 para usar TreeSet");
+		
+		//IOS
+		System.out.println("Presione 3 para usar LinkedHashSet");
+		int opcion = s.nextInt();
+		
+		
+		FactoryDesarrolladores.Setcall(opcion);
+		
+	}
 
 }
--- src/FactoryDesarrollo.java
+++ src/FactoryDesarrollo.java
@@ -1,4 +1,24 @@
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-public class FactoryDesarrollo {
+
+
+public class FactoryDesarrollo<E> {
+
+	public Set<E> Setcall(int opcion) {
+		switch(opcion) {
+		case 1:
+			return new HashSet<E>();
+		case 2:
+			return new TreeSet<E>();
+		case 3:
+			return new LinkedHashSet<E>();
+		default:
+			return null;
+	  }
+
+   }
 
 }
